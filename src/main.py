702230cf0800427285e583a2089279c828d84c2f
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-04-18 01:26:58 +0430

    Add a write_to_json function + various other improvements

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+import json
+
 import requests
 from bs4 import BeautifulSoup
+from joblib import Parallel, delayed
 
-from src.settings import mycol
+from src.settings import target_collection
 
 
 def opac_nlai():
@@ -26,9 +29,45 @@ def opac_nlai():
                 schema[key] = new_val
             else:
                 schema[key] = value
-        mycol.insert_one(schema)
+        target_collection.insert_one(schema)
 
 
+def opac_nlai_write_to_json(range_start, range_end):
+    results = Parallel(n_jobs=8)(delayed(scrape)(i) for i in range(range_start, range_end))
+    results_as_dict = {}
+    preceding_text = 1
+    with open(f"dataset1.json", 'w', encoding='utf-8') as outfile:
+        for i in range(len(results)):
+            results_as_dict[str(preceding_text)] = results[i]
+            preceding_text += 1
+        json.dump(results_as_dict, outfile, indent=4, ensure_ascii=False)
+
+
+def scrape(i):
+    url = f"http://opac.nlai.ir/opac-prod/bibliographic/{i}"
+    print(url)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, features="lxml")
+    form = soup.find("form",
+                     attrs={"name": "search_BrowseSearchHitsForm"})
+    formcontent = form.find('td', attrs={"class": "formcontent"})
+    td = formcontent.find('td', attrs={"width": "100%"})
+    rows = td.find_all('tr')
+    item = {}
+    translatable = str.maketrans('', '', '\u200f\u202a\u202b\u202c\u202d\u202e')
+    for row in rows:
+        row_list = row.find_all('td')
+        key = row_list[0].text.translate(translatable)
+        value = row_list[2].text.translate(translatable)
+        if item.get(key) is not None:
+            new_val = item[key] + " " + value
+            item[key] = new_val
+        else:
+            item[key] = value
+    return item
+
+
+# Does not work anymore
 def libs_nlai():
     for i in range(1, 2324306):
         url = f"https://libs.nlai.ir/bibliography/{i}"
@@ -47,9 +86,8 @@ def libs_nlai():
             else:
                 schema[key] = value
         schema['index'] = i
-        mycol.insert_one(schema)
+        target_collection.insert_one(schema)
 
 
 if __name__ == '__main__':
-    # opac_nlai()
-    libs_nlai()
+    opac_nlai_write_to_json(1, 7265830)
--- src/settings.py
+++ src/settings.py
@@ -5,6 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = pymongo.MongoClient(os.getenv('MONGODB_CONN_STRING'))
-db = client[os.getenv('DATABASE_NAME')]
-mycol = db[os.getenv('COLLECTION_NAME')]
+
+def is_not_empty(string):
+    return bool(string and string.strip())
+
+
+conn_string = os.getenv('MONGODB_CONN_STRING')
+db_name = os.getenv('DATABASE_NAME')
+collection_name = os.getenv('COLLECTION_NAME')
+target_collection = None
+if is_not_empty(conn_string) and is_not_empty(db_name) and is_not_empty(collection_name):
+    client = pymongo.MongoClient(os.getenv('MONGODB_CONN_STRING'))
+    db = client[os.getenv('DATABASE_NAME')]
+    target_collection = db[os.getenv('COLLECTION_NAME')]
