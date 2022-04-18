faa226b882bd394ed94714b39145f538514b29af
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-04-18 07:04:39 +0430

    Remove excess code

--- src/main.py
+++ src/main.py
@@ -6,32 +6,6 @@ import requests
 from bs4 import BeautifulSoup
 from joblib import Parallel, delayed
 
-from settings import target_collection
-
-
-def opac_nlai():
-    for i in range(1, 7265830):
-        url = f"http://opac.nlai.ir/opac-prod/bibliographic/{i}"
-        print(url)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, features="lxml")
-        form = soup.find("form",
-                         attrs={"name": "search_BrowseSearchHitsForm"})
-        formcontent = form.find('td', attrs={"class": "formcontent"})
-        td = formcontent.find('td', attrs={"width": "100%"})
-        rows = td.find_all('tr')
-        schema = {}
-        for row in rows:
-            row_list = row.find_all('td')
-            key = row_list[0].text
-            value = row_list[2].text
-            if schema.get(key) is not None:
-                new_val = schema[key] + " " + value
-                schema[key] = new_val
-            else:
-                schema[key] = value
-        target_collection.insert_one(schema)
-
 
 def opac_nlai_write_to_json(range_start, range_end):
     results = Parallel(n_jobs=8)(delayed(scrape)(i) for i in range(range_start, range_end))
@@ -66,28 +40,6 @@ def scrape(i):
         else:
             item[key] = value
     return item
-
-
-# Does not work anymore
-def libs_nlai():
-    for i in range(1, 2324306):
-        url = f"https://libs.nlai.ir/bibliography/{i}"
-        print(url)
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, features="lxml")
-        table = soup.find('tbody')
-        rows = table.find_all('tr')
-        schema = {}
-        for row in rows:
-            key = row.find('th').text
-            value = row.find('td').text
-            if schema.get(key) is not None:
-                new_val = schema[key] + "," + value
-                schema[key] = new_val
-            else:
-                schema[key] = value
-        schema['index'] = i
-        target_collection.insert_one(schema)
 
 
 if __name__ == '__main__':
